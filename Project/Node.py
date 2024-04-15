dfe13bb6df5d0106861a78b050ed7cbc46018593
JaviGlez <javiergonzavelazquez@gmail.com>
2024-04-15 13:03:32 +0100

    Actualización clase Node.py

--- Project/Node.py
+++ Project/Node.py
@@ -1,7 +1,14 @@
 class Node:
-    def __init__(self, key, color='black'):
+    def __init__(self, key, color='red'):
         self.key = key
         self.parent = None
         self.left = None
         self.right = None
         self.color = color
+
+
+class ReedBlackTree:
+    def __init__(self):
+        self.NIL = Node(None)
+        self.NIL.color = 'black'
+        self.root = self.NIL
